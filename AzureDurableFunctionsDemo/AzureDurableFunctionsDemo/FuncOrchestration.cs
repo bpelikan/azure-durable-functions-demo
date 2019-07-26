0168bdf561c8286f1f0be28f64258fd5a580fa93
bpelikan <bartoszpelikan@gmail.com>
2019-07-26 20:06:58 +0200

    First function

---
+++ AzureDurableFunctionsDemo/AzureDurableFunctionsDemo/FuncOrchestration.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureDurableFunctionsDemo
+{
+    public static class FuncOrchestration
+    {
+        [FunctionName("FuncOrchestration")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("FuncOrchestration_Hello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("FuncOrchestration_Hello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("FuncOrchestration_Hello", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("FuncOrchestration_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
+        }
+
+        [FunctionName("FuncOrchestration_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("FuncOrchestration", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
